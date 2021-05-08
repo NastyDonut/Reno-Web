fbdcb753966beefe16a2652eb4ed7efafe23e287
jessicatorres-debug <jes.tores_2277.jt@gmail.com>
2021-05-08 10:35:25 -0500

    Initial Set-up and Installation of Dependecies

---
+++ app/src/Components/App/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+const App = () => {
+  return (
+    <div>
+      <div>
+      <Button variant="contained"> b</Button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
