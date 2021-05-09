3c61ae43e9ed349b8e52cc2b83bd1f6250ed7453
jessicatorres-debug <jes.tores_2277.jt@gmail.com>
2021-05-08 21:26:54 -0500

    Set up the functions to create user document and retrieve user document

--- app/src/firebase/firebase.js
+++ app/src/firebase/firebase.js
@@ -5,9 +5,6 @@ import "firebase/firestore";
 
 
 
-const provider = new firebase.auth.GoogleAuthProvider();
-
-
 // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
 const firebaseConfig = {
   apiKey: "AIzaSyA2Q_ogamOcLnJi-zUf2TeRfs96F0wxBNE",
@@ -22,8 +19,56 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
-export const signInWithGoogle = () => {
+const provider = new firebase.auth.GoogleAuthProvider();
+
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const signInWithGoogle = () => {
   auth.signInWithPopup(provider);
 };
+
+
+const generateUserDocument = async (user, additionalData) => {
+  if (!user) return;
+  const userRef = firestore.doc(`users/${user.uid}`);
+  const snapshot = await userRef.get();
+  if (!snapshot.exists) {
+    const { email, displayName, photoURL } = user;
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        photoURL,
+        ...additionalData
+      });
+    } catch (error) {
+      console.error("Error creating user document", error);
+    }
+  }
+  return getUserDocument(user.uid);
+};
+
+
+const getUserDocument = async uid => {
+  if (!uid) return null;
+  try {
+    const userDocument = await firestore.doc(`users/${uid}`).get();
+    return {
+      uid,
+      ...userDocument.data()
+    };
+  } catch (error) {
+    console.error("Error fetching user", error);
+  }
+};
+
+
+export {
+  auth,
+  firestore,
+  signInWithGoogle,
+  generateUserDocument,
+  getUserDocument
+}
+
