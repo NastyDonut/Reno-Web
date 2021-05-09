e7d33b43b6054d4ee8b4312dc2bc7e88073ed2f2
jessicatorres-debug <jes.tores_2277.jt@gmail.com>
2021-05-08 21:34:36 -0500

    Update the componentDidMount in UserProvider component

--- app/src/providers/UserProvider.jsx
+++ app/src/providers/UserProvider.jsx
@@ -1,5 +1,6 @@
 import React, { Component, createContext } from "react";
-import { auth } from '../firebase/firebase';
+
+import { auth, generateUserDocument } from '../firebase/firebase';
 
 export const UserContext = createContext({ user: null });
 
@@ -11,9 +12,10 @@ class UserProvider extends Component {
     user: null
   };
 
-  componentDidMount = () => {
-    auth.onAuthStateChanged(userAuth => {
-      this.setState({ user: userAuth});
+  componentDidMount = async () => {
+    auth.onAuthStateChanged(async userAuth => {
+      const user = await generateUserDocument(userAuth);
+      this.setState({ user });
     });
   };
 
