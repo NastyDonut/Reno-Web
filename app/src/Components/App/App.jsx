52efd691502b5899791b8872444eaabe292c60d8
jessicatorres-debug <jes.tores_2277.jt@gmail.com>
2021-05-08 20:48:14 -0500

    Create a providers folder and set up for firebase auth paired with Context API

--- app/src/Components/App/App.jsx
+++ app/src/Components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Router } from "@reach/router";
+import UserProvider from '../providers/UserProvider';
 import SignIn from "../SignIn";
 import SignUp from "../SignUp";
 import ProfilePage from "../ProfilePage";
@@ -12,18 +13,18 @@ import PassReset from "../PassReset";
 const App = () => {
   const user = null;
 
-
   return (
-    user ?
-    <ProfilePage />
-  :
-    <Router>
-      <SignUp path="signUp" />
-      <SignIn path="/" />
-      <PassReset path = "passwordReset" />
+    <UserProvider>
+      user ?
+      <ProfilePage />
+    :
+      <Router>
+        <SignUp path="signUp" />
+        <SignIn path="/" />
+        <PassReset path = "passwordReset" />
 
-    </Router>
-
+      </Router>
+    </UserProvider>
   );
 };
 
